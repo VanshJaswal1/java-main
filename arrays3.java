1883eef8bdad1c595195b6718ececdd8350dee85
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-30 11:15:01 +0530

    add them

---
+++ arrays3.java
@@ -0,0 +1,18 @@
+public class arrays3 {
+    public static void main(String[] args) {
+         int [] arr = { 22,44,555,666,777};
+         int sum = 0 ;
+         for( int i = 0 ; i<5 ;i++)
+          {
+            sum+=arr[i];
+          }
+        
+         System.out.println(" the summ of the emnts is " + (sum));
+         float average = sum /5;
+         System.out.println(average);
+        
+        
+        
+        }
+    
+}

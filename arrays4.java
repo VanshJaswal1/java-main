300e897bca20e7fc391e51f1b7c1646a678db7f9
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-30 11:41:09 +0530

    addd themmmm

---
+++ arrays4.java
@@ -0,0 +1,10 @@
+public class arrays4 {
+    public static void main(String[] args) {
+        System.out.println("reversing an array in java");
+
+        int [] arr = { 1,2,3,4,5,6};
+         for( int i = 5 ; i>=0 ; i--) {
+            System.out.println( arr[i]);
+         }
+    }
+}

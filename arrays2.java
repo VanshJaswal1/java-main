7e77fd7cb9846a929c09e1b950073fd257922172
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-30 10:38:19 +0530

    add them

---
+++ arrays2.java
@@ -0,0 +1,8 @@
+public class arrays2 {
+     public static void main(String[] args) {
+         int [] arr = { 2,3,4,5,65};
+         for(int i = 0 ; i <5 ;i++) {
+            System.out.println(arr[i]);
+         }
+     }
+}

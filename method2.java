a2f41ede27f4376af073226aed5926a211e1ec1a
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-07-03 10:48:27 +0530

    addd them

---
+++ method2.java
@@ -0,0 +1,34 @@
+public class method2 {
+     // with parameter
+    int compare (  int x ,int y ) {
+       
+         int c;
+         if( x>y) {
+            c =  x+y;
+         }
+         else {
+            c = x - y;
+         }
+       return c;
+    }
+    // static method  
+    static char grade ( char z  ) {
+        
+        if ( z == 'A' ) {
+            System.out.println("it worked");
+        } 
+        else{
+            System.out.println("it did not worked");
+        }
+        return z;
+    }
+    public static void main(String[] args) {
+        System.out.println( " instance method");
+
+        method2 obj1 = new method2();
+         int sum = obj1.compare( 4,5);
+         System.out.println(sum);
+        
+         method2.grade('A');
+    }
+}

9c77cc23011f6c9b985dfdfdb73d457ff3313d23
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-07-05 11:29:54 +0530

    adddd

---
+++ recursion.java
@@ -0,0 +1,16 @@
+public class recursion {
+         // using method 
+
+         static void loop ( int n ) {
+            
+            if ( n == 10)
+            {
+                return;
+            }
+      System.out.println(n);
+
+         }
+     public static void main(String[] args) {
+        loop(1);
+     }
+        }

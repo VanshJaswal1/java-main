7244a828a1470e05ec3d5dabd1d129daccfe28a8
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-07-17 12:54:29 +0530

    addd them

--- oops6.java
+++ oops6.java
@@ -1,6 +1,29 @@
 //  ||   CONSTRUCTORS   ||
 
+ 
+    
+ class student {
+    String name;
+    int age;
+
+          student() {
+              name = "vansh";
+              age = 22;
+          }
+ 
+    void display(){
+    System.out.println(" my first basic constructor code ");
+ }
+ 
+}
+ 
 
 public class oops6 {
-    
+      public static void main(String[] args) {
+         System.out.println(" helo world");
+
+         student s1 = new student();
+         s1.display();
+         System.out.println(s1.name + s1.age);
+      }
 }

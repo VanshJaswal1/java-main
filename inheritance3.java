dfb10cb155e6811e9c872b3806a71ee73884de85
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-07-19 08:58:38 +0530

    add them

---
+++ inheritance3.java
@@ -0,0 +1,18 @@
+class animal {
+    animal () {
+        System.out.println("animal makes differnt sounds");
+    }
+}
+class dog extends animal {
+    dog() {
+        System.out.println("dog makes souds like barkss");
+    }
+}
+
+public class inheritance3 {
+    public static void main(String[] args) {
+    
+        dog d = new dog();
+        
+    }
+}

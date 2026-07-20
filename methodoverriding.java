2e951908bd40b6370ff4377d84673c5b2f79fd70
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-07-20 15:16:08 +0530

    adddd them

--- methodoverriding.java
+++ methodoverriding.java
@@ -1,3 +1,18 @@
+class animal {
+    void sound() {
+        System.out.println("animal can makes sound");
+    }
+}
+class dog extends animal {
+    // @Override
+    void sound() {
+        System.out.println("dog makes sound like barkj");
+    }
+}
+
 public class methodoverriding {
-    
+    public static void main(String[] args) {
+        dog d = new dog();
+        d.sound();
+    }
 }

d42551fe433f65676b04fb132689b6c1a3e7e3ff
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-08-11 23:01:02 +0530

    add them

--- Interface1.java
+++ Interface1.java
@@ -6,21 +6,29 @@
 interface fees {
     void moneyget(int b);
 }
-
+ interface salary {
+    void moneytake(int c);
+ }
    
- class teacher implements school {
+ class teacher implements school, salary {
      public void members(int a){
          System.out.println("the number of teachers are " + " " + a );
     };
+    public  void moneytake(int c) {
+        System.out.println(" the monet the get is  " + " " + c );
+    }
  }
  class student implements school {
      public void members(int a){
          System.out.println("the number of student are " + " " + a );
     };
+    public void fees (int b ) {
+        System.out.println(" the fees studernt pays to the  school is " + " " + b);
+    }
  }
 
 public class Interface1 {
      public static void main(String[] args) {
-        
+         System.out.println(" an example of interface ");
      }
 }

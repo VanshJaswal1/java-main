7efa49e79956d778ea8ef26a01faa207de043dce
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-08-11 22:18:06 +0530

    addd them

---
+++ Abstract1.java
@@ -0,0 +1,27 @@
+abstract class vechicle {
+
+    abstract void start();
+
+}
+class car extends vechicle {
+      void start() {
+        System.out.println("Car starts");
+}
+class bus extends vechicle {
+      void start() {
+        System.out.println("bus starts");
+}
+
+}
+public class Abstract1 {
+
+    public static void main(String[] args) {
+              System.out.println("just a code ");
+            car obj1 = new car();
+            bus obj2 = new bus();
+            obj1.start();
+            obj2.start();
+    }
+
+}
+}

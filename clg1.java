92a3a8cb9032146f2b921108e78f19dbdd1c9eb0
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-08-22 13:03:16 +0530

    add themm

---
+++ clg1.java
@@ -0,0 +1,18 @@
+public class clg1 {
+    public static void main(String[] args) {
+        String name = "vansh Jaswal";
+        int age = 20;
+        char grade = 'A';
+        boolean isSingle = true;
+        float marks = 90.00f;
+        double favnumber = 200.637464700;
+       System.out.println(" about me ");
+       System.out.println(" the name of the boy is  = " + " = " + name );      
+       System.out.println(" the age of the boy is  = " + " = " + age  );      
+       System.out.println(" the grade of the boy is  = " + " = " + grade );      
+       System.out.println(" is the mf is single or not   = " + " = " + isSingle);
+       System.out.println(" the marks  of the boy is  = " + " = " + marks );
+       System.out.println(" the fav number  of the boy is  = " + " = " + favnumber );                  
+        
+    }
+}
